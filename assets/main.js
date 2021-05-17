af9ac1b576a35f26cefcfcaa8e22edc01f50bc53
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-17 14:34:22 +0200

    complete ex

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+let app = new Vue({
+    el:"#root",
+        data:{
+            message: "Hello VUE",
+            //Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+            imgPath:"./assets/img/lorem-picsum.jpg"
+            
+        }
+})
